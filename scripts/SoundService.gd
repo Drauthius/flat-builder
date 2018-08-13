0fe18de43c24e3176f9e54044e78b0255ee369ee
worai <42282410+worai@users.noreply.github.com>
2018-08-13 21:09:54 +0200

    commented out music
    commented out music from sound service class

--- scripts/SoundService.gd
+++ scripts/SoundService.gd
@@ -9,8 +9,8 @@ var block_placement_finalise_player = AudioStreamPlayer.new()
 var physics_start_player = AudioStreamPlayer.new()
 var physics_pause_player = AudioStreamPlayer.new()
 
-var basic_music_loop01_player = AudioStreamPlayer.new()
-var basic_music_loop02_player = AudioStreamPlayer.new()
+#var basic_music_loop01_player = AudioStreamPlayer.new()
+#var basic_music_loop02_player = AudioStreamPlayer.new()
 
 func _ready():
 	beam_placement_selection_player.stream = preload("res://music/beam_placement_selection.wav")
@@ -19,7 +19,7 @@ func _ready():
 	block_placement_selection_player.stream = preload("res://music/block_placement_selection.wav")
 	block_placement_finalise_player.stream = preload("res://music/block_placement_finalise.wav")
 	
-	physics_start_player.stream = preload("res://music/physics_start.wav")
+#	physics_start_player.stream = preload("res://music/physics_start.wav")
 
 func attach_audiostream_players(node):
 	node.add_child(beam_placement_selection_player)
@@ -27,8 +27,8 @@ func attach_audiostream_players(node):
 	node.add_child(block_placement_selection_player)
 	node.add_child(block_placement_finalise_player)
 	node.add_child(physics_start_player)
-	node.add_child(basic_music_loop01_player)
-	node.add_child(basic_music_loop02_player)
+#	node.add_child(basic_music_loop01_player)
+#	node.add_child(basic_music_loop02_player)
 
 func beam_placement_selection():
 	beam_placement_selection_player.play()
@@ -42,8 +42,8 @@ func block_placement_selection_():
 func block_placement_finalise():
 	block_placement_finalise_player.play()
 
-func basic_music_loop01():
-	basic_music_loop01_player.play()
-
-func basic_music_loop02():
-	basic_music_loop02_player.play()
+#func basic_music_loop01():
+#	basic_music_loop01_player.play()
+#
+#func basic_music_loop02():
+#	basic_music_loop02_player.play()
